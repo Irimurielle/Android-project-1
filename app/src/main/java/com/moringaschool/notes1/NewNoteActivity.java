205deee913ae9b3d94db72b53deeacef62538f38
Irimurielle <irimurielle@gmail.com>
2020-12-21 12:31:20 +0200

    fixing button errors

--- app/src/main/java/com/moringaschool/notes1/NewNoteActivity.java
+++ app/src/main/java/com/moringaschool/notes1/NewNoteActivity.java
@@ -2,9 +2,15 @@ package com.moringaschool.notes1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewNoteActivity extends AppCompatActivity {
+    public static final String TAG = Activity.class.getSimpleName();
+    private EditText mNoteEditText;
+    private Button mSaveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

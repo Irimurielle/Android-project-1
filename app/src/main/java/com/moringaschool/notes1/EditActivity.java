9664b2ce9b631506348f1a9aeb8d1b8ff48a81c9
Irimurielle <irimurielle@gmail.com>
2020-12-21 10:20:17 +0200

    added intent

--- app/src/main/java/com/moringaschool/notes1/EditActivity.java
+++ app/src/main/java/com/moringaschool/notes1/EditActivity.java
@@ -2,13 +2,23 @@ package com.moringaschool.notes1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import static com.moringaschool.notes1.MainActivity.*;
 
 public class EditActivity extends AppCompatActivity {
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        mEditText = (EditText) findViewById(R.id.editText);
+        Intent intent = getIntent();
+        String notes = intent.getStringExtra("notes");
+        mEditText.setText(MainActivity.notes);
     }
 }

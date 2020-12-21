c23aefd6082ea78758ed0bd17b0c6f692417127d
Irimurielle <irimurielle@gmail.com>
2020-12-21 09:39:29 +0200

    added listview

--- app/src/main/java/com/moringaschool/notes1/MainActivity.java
+++ app/src/main/java/com/moringaschool/notes1/MainActivity.java
@@ -3,12 +3,16 @@ package com.moringaschool.notes1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listView);
     }
 }

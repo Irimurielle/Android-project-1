374f7fbcd79b905500994bc26038f9ba05590054
Irimurielle <irimurielle@gmail.com>
2020-12-21 10:00:03 +0200

    added intent

--- app/src/main/java/com/moringaschool/notes1/MainActivity.java
+++ app/src/main/java/com/moringaschool/notes1/MainActivity.java
@@ -2,13 +2,16 @@ package com.moringaschool.notes1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     private ListView mListView;
-    static String[] tasks = new String[] {"Drinking water each day at 8:00 Am", "Reading the Bible each night before going to bed"};
+    static String[] notes = new String[] {"Christmas Decoration"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mListView = (ListView) findViewById(R.id.listView);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tasks);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
         mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                intent.putExtra("notes", notes);
+                startActivity(intent);
+            }
+        });
+
     }
 }
